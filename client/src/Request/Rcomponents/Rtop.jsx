fb9bbf2f440eba9123176c3654f9ef1264dd6d1b
Deshan-N-Gimhara <87584743+Deshan-N-Gimhara@users.noreply.github.com>
2024-01-22 17:07:43 +0530

    fix: component naming

--- client/src/Request/Rcomponents/Rtop.jsx
+++ client/src/Request/Rcomponents/Rtop.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Input from "../../AdminPage/Components/Input";
 import axios from "axios";
 
-function Rtop() {
+function RequestTop() {
   const [formData, setFormData] = useState({
     examName: "",
     examDate: "",
@@ -36,49 +36,49 @@ function Rtop() {
   };
 
   return (
-    <div className="w-full pb-10 ">
-      <div className="w-[60%] mx-auto justify-center">
-        <h1 className="text-black text-[25px] font-normal font-Lato text-center flex ">
+    <div className='w-full pb-10 '>
+      <div className='w-[60%] mx-auto justify-center'>
+        <h1 className='text-black text-[25px] font-normal font-Lato text-center flex '>
           If you have an emergency, you can request for an exchange of duty and
           suggest a lecturer for the examination duty.
         </h1>
-        <div className="flex justify-center mx-auto">
+        <div className='flex justify-center mx-auto'>
           <form onSubmit={handleSubmit}>
-            <div className="w-[760px] h-[420px] bg-gray-200 rounded-3xl mt-10">
-              <h1 className="text-gray-600 text-[25px] font-normal font-Lato w-[80%] flex justify-center mx-auto py-5">
+            <div className='w-[760px] h-[420px] bg-gray-200 rounded-3xl mt-10'>
+              <h1 className='text-gray-600 text-[25px] font-normal font-Lato w-[80%] flex justify-center mx-auto py-5'>
                 Request for an exchange and suggest a lecturer
               </h1>
-              <div className="flex w-[80%] mt-[10px] mx-auto justify-center space-x-[20%]">
+              <div className='flex w-[80%] mt-[10px] mx-auto justify-center space-x-[20%]'>
                 <div>
-                  <p className="text-black text-[20px]  font-normal font-Lato">
+                  <p className='text-black text-[20px]  font-normal font-Lato'>
                     <Input
-                      title="Examination Name"
-                      type="text"
-                      name="examName"
-                      display="Enter exam code"
+                      title='Examination Name'
+                      type='text'
+                      name='examName'
+                      display='Enter exam code'
                       onChange={handleChange}
                     />
                   </p>
                 </div>
-                <p className="text-black text-[20px] font-normal font-Lato">
+                <p className='text-black text-[20px] font-normal font-Lato'>
                   <Input
-                    title="Examination Date"
-                    type="date"
-                    name="examDate"
-                    display="enter Date"
+                    title='Examination Date'
+                    type='date'
+                    name='examDate'
+                    display='enter Date'
                     onChange={handleChange}
                   />
                 </p>
               </div>
-              <div className="ml-[21%] mt-[1%]">
-                <p className="text-black text-[23px] font-normal font-Lato">
+              <div className='ml-[21%] mt-[1%]'>
+                <p className='text-black text-[23px] font-normal font-Lato'>
                   Can you Suggest a lecturer ?
                 </p>
-                <div className="flex space-x-5 w-[20%] ">
+                <div className='flex space-x-5 w-[20%] '>
                   <div>
                     <input
-                      type="checkbox"
-                      name="suggestLecturer"
+                      type='checkbox'
+                      name='suggestLecturer'
                       value={formData.suggestLecturer}
                       onChange={handleChange}
                     />
@@ -86,21 +86,21 @@ function Rtop() {
                   </div>
                 </div>
               </div>
-              <p className="text-black text-[15px] font-normal font-Lato mt-[2%] ml-[21%]">
+              <p className='text-black text-[15px] font-normal font-Lato mt-[2%] ml-[21%]'>
                 *Click here to notify the selected lecturer
               </p>
               {formData.suggestLecturer && (
-                <div className="flex justify-center ">
+                <div className='flex justify-center '>
                   <input
-                    className="  mt-[1%] w-[500px] h-[50px] bg-white border border-gray-400 rounded-lg"
-                    type="text"
-                    name="lecturerName"
-                    placeholder="Name of the Lecturer you suggest"
+                    className='  mt-[1%] w-[500px] h-[50px] bg-white border border-gray-400 rounded-lg'
+                    type='text'
+                    name='lecturerName'
+                    placeholder='Name of the Lecturer you suggest'
                     onChange={handleChange}
                   />
                 </div>
               )}
-              <button className="flex justify-center mx-auto text-[20px] font-normal font-Lato mt-5  relative px-4 py-1 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-purple-600">
+              <button className='flex justify-center mx-auto text-[20px] font-normal font-Lato mt-5  relative px-4 py-1 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-purple-600'>
                 Request
               </button>
             </div>
@@ -111,4 +111,4 @@ function Rtop() {
   );
 }
 
-export default Rtop;
+export default RequestTop;
